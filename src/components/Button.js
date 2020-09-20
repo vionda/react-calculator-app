b8051f01659e5567332b94e7ed2b600f90c74f84
vionda <viondasalihu@gmail.com>
2020-09-21 00:01:44 +0200

    calculator

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    return (
+      <div className={`column-${this.props.cols}`}>
+        <button
+          onClick={() => this.props.action(this.props.symbol)}
+          className="calc-button"
+        >
+          {this.props.symbol}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Button;
